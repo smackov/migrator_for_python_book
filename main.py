9d00fb3b9ff9a0ecf69bef72744bdc262aa78dff
smackov <smackovdinislam@gmail.com>
2021-03-06 19:14:05 +0500

    Del redundant func 'set_environ' from 'main.py' and other little changes

--- main.py
+++ main.py
@@ -10,17 +10,12 @@ import sys
 import django
 
 import settings
+import db
 from lookup_dirs import lookup_dirs
-from db import clear_db, populate_db
-from environment import set_environ
 
 
 # The run part
 if __name__ == '__main__':
-    # Set settings for Django Project
-    set_environ()
-    # Start article migration
     chapters, sections, articles = lookup_dirs(root_path = settings.PATH_TO_CONTENT)
-    print(articles[0].name)
-    clear_db()
-    populate_db(chapters=chapters, sections=sections, articles=articles)
+    db.clear_db()
+    db.populate_db(chapters=chapters, sections=sections, articles=articles)
